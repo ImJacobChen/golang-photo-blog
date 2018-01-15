0a18b8e054fd0f7a3c0be808ddf84e144765402e
Jacob Chen <jacob.chen@2112comms.com>
2018-01-15 19:00:50 +0000

    Create a cookie when hitting route: index and then print out the values in the cookie

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
@@ -17,6 +21,47 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	c := getCookie(w, r)
+	c = appendValue(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
+}
+
+// get cookie
+func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID, err := uuid.NewV4()
+		if err != nil {
+			log.Println(err)
+		}
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
 }
