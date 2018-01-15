2d25cb40ecb4da2769d6e8d273fc119e9a40bb98
Jacob Chen <jacob.chen@2112comms.com>
2018-01-15 19:13:50 +0000

    Process file and add filename to cookie

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 
 	uuid "github.com/satori/go.uuid"
@@ -23,7 +28,36 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	c := getCookie(w, r)
-	c = appendValue(w, c)
+
+	// process form
+	if r.Method == http.MethodPost {
+		mf, fh, err := r.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		// create sha for file name
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		// create new file
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copy
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		// add filename to user cookie
+		c = appendValue(w, c, fname)
+	}
 	xs := strings.Split(c.Value, "|")
 	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
@@ -45,22 +79,11 @@ func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
 	return c
 }
 
-func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
-	p1 := "disneyland.jpg"
-	p2 := "atbeach.jpg"
-	p3 := "hollywood.jpg"
-
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
 	s := c.Value
-	if !strings.Contains(s, p1) {
-		s += "|" + p1
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
 	}
-	if !strings.Contains(s, p2) {
-		s += "|" + p2
-	}
-	if !strings.Contains(s, p3) {
-		s += "|" + p3
-	}
-
 	c.Value = s
 	http.SetCookie(w, c)
 	return c
